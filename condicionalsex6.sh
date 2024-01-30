00d68feb6473003917b3c0f2210919829841359f
dgimenez20 <158026468+dgimenez20@users.noreply.github.com>
2024-01-30 10:01:57 +0100

    Update condicionalsex6.sh

--- condicionalsex6.sh
+++ condicionalsex6.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 read -p  "Introdueix una ruta d'un fitxer: " file
-
+#miro si el fitxer existeix
 if [ -e $file ]
+#si existeix faig un ls -l per mostrar permisos
 then
    ls -l $file
+#sino existeix mostro error
 else
    echo "L'arxiu NO existeix"
 fi
