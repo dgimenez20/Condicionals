fb4339cfbce0c9a4978773101f910a31af4a3b34
dgimenez20 <158026468+dgimenez20@users.noreply.github.com>
2024-01-30 09:54:24 +0100

    Update condicionalsex1.sh

--- condicionalsex1.sh
+++ condicionalsex1.sh
@@ -2,13 +2,15 @@
 
 read -p  "Introdueix un numero: " num
 
-
+#miro si el numero es mes gran que 0
 if [ $num -gt '0' ]
 then
     echo "El numero es positiu"
+#miro si el numero es mes petit que 0
 elif [ $num -lt '0' ]
 then
     echo "El numero es negatiu"
+#si no es cap de les dues el numero es 0
 else
     echo "El numero es 0"
 fi
