73ba73aa31b1f72046c6e2b01f964485f3b10de5
dgimenez20 <158026468+dgimenez20@users.noreply.github.com>
2024-01-30 10:05:06 +0100

    Update condicionalsex8.sh

--- condicionalsex8.sh
+++ condicionalsex8.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 read -p  "Introdueix una ruta d'un directori: " file
-
+#miro si el directori existeix
 if [ -d $file ]
+#si existeix comprimeixo el directori
 then
    tar -czvf directori_comprimit.tar.gz $file
+#sino mostro error
 else
    echo "El directori NO existeix"
 fi
