cf0a694cc789ea01bcb019a4065bff88ce3a52d3
dgimenez20 <158026468+dgimenez20@users.noreply.github.com>
2024-01-30 09:57:21 +0100

    Update condicionalsex5.sh

--- condicionalsex5.sh
+++ condicionalsex5.sh
@@ -2,10 +2,11 @@
 
 read -p  "Introdueix una ruta: " path
 
-
+#comprovo si la ruta existeix
 if [ -e $path ]
 then
    echo "L'arxiu "$path "existeix"
+#sino existeix printo l'error
 else
    echo "L'arxiu "$path "NO existeix"
 fi
