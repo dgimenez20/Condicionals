714070739e784ee663f7afe5420b470d1c0b53ad
dgimenez20 <158026468+dgimenez20@users.noreply.github.com>
2024-01-30 09:56:39 +0100

    Update condicionalsex4.sh

--- condicionalsex4.sh
+++ condicionalsex4.sh
@@ -2,10 +2,11 @@
 
 read -p  "Introdueix un numero: " num
 
-
+#miro si el numero es igual a 3
 if [ $num = '3' ]
 then
     echo "El numero es 3"
+#sino es 3 printo el error
 else
     echo "ERROR EL NUMERO INTRODUIT NO ES 3"
 fi
