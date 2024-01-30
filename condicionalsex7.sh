e6bff598c3edd60aa36ba98cae175fc05531185a
dgimenez20 <158026468+dgimenez20@users.noreply.github.com>
2024-01-30 10:04:07 +0100

    Update condicionalsex7.sh

--- condicionalsex7.sh
+++ condicionalsex7.sh
@@ -2,11 +2,14 @@
 
 read -p  "Introdueix una ruta d'un fitxer: " file
 
+#miro si la ruta existeix
 if [ -e $file ]
 then
+#miro si es un directori
    if [ -d $file ]
    then
       echo "Es un dirctori"
+#sino dic que es un fitxer i mostro la extensió del fitxer
    else
       echo "Es un fitxer i la extensio es "${file##*.}
    fi
