7d32ec331a19c1add63300d89dfeed83fe1b4d92
dgimenez20 <158026468+dgimenez20@users.noreply.github.com>
2024-01-30 10:08:08 +0100

    Update condicionalsex9.sh

--- condicionalsex9.sh
+++ condicionalsex9.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 read -p  "Introdueix un protocol: " pro
-
+#faig un grep de totes les vegades que apareix la paraula i ho rederigeixo a un nou fitxer 
 grep $pro /home/david/Escritorio/wireshark.txt >> lineas.txt
+#compto les lineas del nou fitxer
 wc -l lineas.txt
